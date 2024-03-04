98fc9baa62c35f7613fd0cd7a3016a7192c26bbe
edi vidrascu <edividrascu@gmail.com>
2024-03-04 10:04:20 +0100

    ejercicio 4

--- dump.sqlite.sql
+++ dump.sqlite.sql
@@ -26,9 +26,9 @@ MODIFY COLUMN book_id INT AUTO_INCREMENT;
 INSERT INTO Libros (titulo, autor, genero, ano_publicado, isbn, precio, calificacion, stock_count, editor, num_paginas)
 VALUES ('Un viaje al centro', 'Julio Verne', 'Aventura', 1864, '1234567890123', 12.99, 4.5, 10, 'Editorial Verne', 350);
 
--- "Guerra y paz" de León Tolstoi
+-- "War and Peace" de León Tolstoi
 INSERT INTO Libros (titulo, autor, genero, ano_publicado, isbn, precio, calificacion, stock_count, editor, num_paginas)
-VALUES ('Guerra y paz', 'León Tolstoi', 'Histórico', 1869, '1234567890124', 14.99, 4.8, 5, 'Impresiones de Tolstoi', 1200);
+VALUES ('War and Peace', 'León Tolstoi', 'Histórico', 1869, '1234567890124', 14.99, 4.8, 5, 'Impresiones de Tolstoi', 1200);
 
 -- "Susurros del viento" de Amelia Blackburn
 INSERT INTO Libros (titulo, autor, genero, ano_publicado, isbn, precio, calificacion, stock_count, editor, num_paginas)
@@ -37,3 +37,9 @@ VALUES ('Susurros del viento', 'Amelia Blackburn', 'Romance', 1982, '12345678901
 -- "La Odisea Galáctica" de Orion Starfield
 INSERT INTO Libros (titulo, autor, genero, ano_publicado, isbn, precio, calificacion, stock_count, editor, num_paginas)
 VALUES ('La Odisea Galáctica', 'Orion Starfield', 'Ciencia ficción', 2005, '1234567890126', 19.99, 4.9, 15, 'Prensa Nebulosa', 450);
+
+-- ejercicio 4 update price
+UPDATE Books
+SET price = 12.99, stock_count = stock_count - 1
+WHERE title = 'War and Peace';
+
